ac24c839dba71874a75412894ce4543f02a57fe0
Eriq Augustine <eriq.augustine@gmail.com>
2020-07-06 12:58:32 -0700

    Added some more stats to the fetch script.

--- mnist-sequence/scripts/fetchResults.sh
+++ mnist-sequence/scripts/fetchResults.sh
@@ -3,8 +3,8 @@
 readonly THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 readonly BASE_OUT_DIR="${THIS_DIR}/../results"
 
-readonly HEADER="Batch Size\tLearning Rate\tEpochs\tCompute Period\tRule Set\tADMM Iterations\tCategorical Accuracy"
-readonly PATTERN='s#^.*/\([0-9\.]\+\)::Batch_Size/\([0-9\.]\+\)::Learning_Rate/\([0-9\.]\+\)::Epochs/\([0-9\.]\+\)::Compute_Period/\(single\|triple\)::Rule_Set/\([0-9\.]\+\)::ADMM_Iterations.*Categorical Accuracy: \([0-9\.]\+\)$#\1\t\2\t\3\t\4\t\5\t\6\t\7#'
+readonly HEADER="Batch Size\tLearning Rate\tEpochs\tCompute Period\tRule Set\tADMM Iterations\tBin Rank Lower\tBin Rank Upper\tBin Threshold Lower\tBin Threshold Upper\tCategorical Accuracy"
+readonly PATTERN='s#^.*/([\d\.]+)::Batch_Size/([\d\.]+)::Learning_Rate/([\d\.]+)::Epochs/([\d\.]+)::Compute_Period/(single|triple)::Rule_Set/([\d\.]+)::ADMM_Iterations/([\d\.]+)::Bin_Rank_Lower/([\d\.]+)::Bin_Rank_Upper/([\d\.]+)::Bin_Threshold_Lower/([\d\.]+)::Bin_Threshold_Upper/.*Categorical Accuracy: ([\d\.]+).*$#$1\t$2\t$3\t$4\t$5\t$6\t$7\t$8\t$9\t$10\t$11#'
 
 function main() {
     if [[ ! $# -eq 0 ]]; then
@@ -15,7 +15,7 @@ function main() {
     trap exit SIGINT
 
     echo -e "${HEADER}"
-    grep -R "Evaluation results for PREDICTEDNUMBER" "${BASE_OUT_DIR}" | sed "$PATTERN"
+    grep -R "Evaluation results for PREDICTEDNUMBER" "${BASE_OUT_DIR}" | perl -pe "$PATTERN"
 }
 
 [[ "${BASH_SOURCE[0]}" == "${0}" ]] && main "$@"
